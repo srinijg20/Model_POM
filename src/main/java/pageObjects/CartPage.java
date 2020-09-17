27c06c79dbc466cceab6912fc9db4953b7967164
unknown <srinijg@gmail.com>
2020-09-17 11:41:11 -0400

    new class

--- src/main/java/pageObjects/CartPage.java
+++ src/main/java/pageObjects/CartPage.java
@@ -4,26 +4,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 public class CartPage {
 	WebDriver driver;
-//	public CartPage(WebDriver driver) {
-//		this.driver = driver;
-//		PageFactory.initElements(driver, this);
-//	}
+
+	public CartPage(WebDriver driver) {
+		// this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
 	@FindBy(how = How.CSS, using = ".cart-button")
 	public WebElement btn_Cart;
 	@FindBy(how = How.CSS, using = ".checkout-button.alt")
 	public WebElement btn_ContinueToCheckout;
-//	public void clickOn_Cart() {
-//		btn_Cart.click();
-//	}
-//
-//	public void clickOn_ContinueToCheckout() {
-//		btn_ContinueToCheckout.click();
-//		try {
-//			Thread.sleep(5000);
-//		} catch (InterruptedException e) {
-//		}
-//	}
+
+	public void clickOn_Cart() {
+		btn_Cart.click();
+	}
+
+	public void clickOn_ContinueToCheckout() {
+		btn_ContinueToCheckout.click();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+		}
+	}
 }
